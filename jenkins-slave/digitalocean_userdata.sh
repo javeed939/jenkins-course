ab3f167f68c2df1b5b58f9febecb3ca02401f525
javeed939 <50522116+javeed939@users.noreply.github.com>
2024-01-22 13:38:17 +0530

    Update digitalocean_userdata.sh

--- jenkins-slave/digitalocean_userdata.sh
+++ jenkins-slave/digitalocean_userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install dependencies
-apt-get install -y \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -9,23 +9,23 @@ apt-get install -y \
     software-properties-common
 
 # get gpg key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 # add docker repo
-add-apt-repository \
+sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
 # update repository
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
-systemctl enable docker
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
 
 # jenkins setup
-mkdir -p /var/jenkins_home/.ssh
-cp /root/.ssh/authorized_keys /var/jenkins_home/.ssh/authorized_keys
-chmod 700 /var/jenkins_home/.ssh
-chmod 600 /var/jenkins_home/.ssh/authorized_keys
-chown -R 1000:1000 /var/jenkins_home
+sudo mkdir -p /var/jenkins_home/.ssh
+sudo cp /root/.ssh/authorized_keys /var/jenkins_home/.ssh/authorized_keys
+sudo chmod 700 /var/jenkins_home/.ssh
+sudo chmod 600 /var/jenkins_home/.ssh/authorized_keys
+sudo chown -R 1000:1000 /var/jenkins_home
 docker run -p 2222:22 -v /var/jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart always -d javeed939/jenkins-slave
